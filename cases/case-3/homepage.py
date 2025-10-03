ac7fd71d79160fcda1a400d0959d66d3eb1b9333
Guus Hulst <guus.hulst@gmail.com>
2025-10-03 15:05:34 +0200

    Updated

--- cases/case-3/homepage.py
+++ cases/case-3/homepage.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+loaded_data = pd.read_pickle("Charging_data.pkl")
+loaded_data2 = pd.read_csv('laadpaaldata.csv')
+
 st.set_page_config(
     page_title="Laadpalen Dashboard",
     page_icon=":sunglasses"
@@ -9,9 +12,6 @@ st.set_page_config(
 
 st.title("This is the homepage")
 st.sidebar.success('Select a page above')
-
-with open("cars.pkl", "rb") as f:
-    loaded_data = pickle.load(f)
 
 if "show_text" not in st.session_state:
     st.session_state.show_text = False
@@ -23,3 +23,4 @@ st.button(label="Toggle Text", on_click=toggle)
 
 if st.session_state.show_text:
     st.write(loaded_data)
+    st.write(loaded_data2)
