47db4ab8facbf9e706f11355fe8b095f47b3ca69
SimoGD <148620178+SimoGD@users.noreply.github.com>
2025-10-04 22:27:10 +0200

    Kleur verandert van choropleth

--- cases/case-3/pages/map.py
+++ cases/case-3/pages/map.py
@@ -6,6 +6,7 @@ from folium.plugins import FastMarkerCluster
 from streamlit_folium import st_folium
 import geopandas as gpd
 from shapely.geometry import Point
+import numpy as np
 
 st.set_page_config(
     page_title="Laadpalen Nederland",
@@ -25,7 +26,7 @@ def fetch_laadpalen(url):
     df['aantal_laadpunten'] = df['NumberOfPoints'].fillna(0).astype(int)
     return df
 
-@st.cache_data(ttl=86400)
+@st.cache_data(ttl=3600)
 def load_gemeenten():
     url = "https://cartomap.github.io/nl/wgs84/gemeente_2023.geojson"
     return gpd.read_file(url)
@@ -41,7 +42,7 @@ def prepare_gemeenten(laadpalen_df, _gemeenten_gdf):
     gemeenten_prepared["aantal_laadpalen"] = gemeenten_prepared["aantal_laadpalen"].fillna(0)
     return gemeenten_prepared
 
-ocm_url = "https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=2000&compact=true&verbose=false&key=93b912b5-9d70-4b1f-960b-fb80a4c9c017"
+ocm_url = "https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=10000&compact=true&verbose=false&key=93b912b5-9d70-4b1f-960b-fb80a4c9c017"
 laadpalen = fetch_laadpalen(ocm_url)
 gemeenten = load_gemeenten()
 
@@ -50,7 +51,9 @@ with col2:
     st.title("Laadpalen in Nederland")
     kaart_keuze = st.radio("Kies welke kaart je wilt zien:", ["Laadpalenkaart", "Laadpalen per gemeente"])
 
-    laadpalen_subset = laadpalen.sample(n=2000, random_state=1)
+    num_points = min(10000, len(laadpalen))
+    laadpalen_subset = laadpalen.sample(n=num_points, random_state=1)
+
 
     m = folium.Map(location=(52.379189, 5), zoom_start=8)
 
@@ -59,17 +62,23 @@ with col2:
         FastMarkerCluster(coords).add_to(m)
     
     else:
-        gemeenten_prepared = prepare_gemeenten(laadpalen, gemeenten)
-        
+        gemeenten_prepared = prepare_gemeenten(laadpalen_subset, gemeenten)
+
+        max_laadpalen = gemeenten_prepared['aantal_laadpalen'].max()
+
+        bins = list(np.linspace(0, max_laadpalen, num=18))
+
         folium.Choropleth(
             geo_data=gemeenten_prepared,
             data=gemeenten_prepared,
             columns=['statnaam', 'aantal_laadpalen'],
             key_on='feature.properties.statnaam',
-            fill_color='YlGn',
+            fill_color='BuGn',
             fill_opacity=0.7,
             line_opacity=0.2,
-            legend_name="Aantal laadpalen per gemeente"
+            legend_name="Aantal laadpalen per gemeente",
+            bins=bins,
+            reset=True
         ).add_to(m)
 
     st_folium(m, width=900, height=1000)
